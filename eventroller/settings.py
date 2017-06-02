d69394146adcc89cda120e63e6c820fb10f7f506
Scott Reynen <scott.reynen@moveon.org>
2017-06-02 12:16:00 -0600

    add huerta and update bootstrapform

--- eventroller/settings.py
+++ eventroller/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'huerta',
+    'bootstrapform',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -68,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'huerta.context_processors.theme_settings',
             ],
         },
     },
