1c5584b42ff3d1b152571f4108c30a6255b074b6
Schuyler Duveen <sky@skyb.us>
2017-06-02 15:32:51 -0400

    review api auth and db-priming

--- reviewer/models.py
+++ reviewer/models.py
@@ -3,9 +3,23 @@ from __future__ import unicode_literals
 from django.db import models
 
 from django.contrib.auth.models import User, Group
-
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+
+from event_store.models import Organization
+
+class ReviewGroup(models.Model):
+  """
+  many-to-many link of what groups can review for what organizations
+  (TODO) Eventually, we may want a workflow that allows someone
+  from one organization to vet events from another for the purposes
+  of copying them over.  In that case, the org would allow sharing (see event_exim app)
+  and wouldn't be added to the other org here, but a user might also be a member
+  of more than one org, and both orgs might be ok with 'sharing' the vetting
+  for the same event (assuming they have the same criteria).
+  """
+  organization = models.ForeignKey(Organization, db_index=True)
+  group = models.ForeignKey(Group, db_index=True)
 
 
 class Review(models.Model):
@@ -18,6 +32,7 @@ class Review(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   modified_at = models.DateTimeField(auto_now=True)
 
+  organization = models.ForeignKey(Organization)
   reviewer = models.ForeignKey(User)
   key = models.CharField(max_length=128,
                          help_text=("the data key for what is being reviewed"
@@ -33,6 +48,7 @@ class ReviewLog(models.Model):
   object_id = models.IntegerField()
   content_object = GenericForeignKey('content_type', 'object_id')
 
+  organization = models.ForeignKey(Organization)
   reviewer = models.ForeignKey(User)
   created_at = models.DateTimeField(auto_now_add=True)
   message = models.TextField()
--- reviewer/views.py
+++ reviewer/views.py
@@ -1,14 +1,15 @@
+from collections import OrderedDict
 import datetime
 import json
 import time
 
 from django.contrib.contenttypes.models import ContentType
-from django.http import HttpResponse, HttpResponseForbidden
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseForbidden, Http404
+from django.shortcuts import render, get_object_or_404
 
 from django_redis import get_redis_connection
 
-from reviewer.models import Review, ReviewLog
+from reviewer.models import Review, ReviewLog, ReviewGroup
 
 """
   This api tries to keep things super-fast using redis datastructures
@@ -25,7 +26,35 @@ from reviewer.models import Review, ReviewLog
    and will leverage django-cachalot
 """
 
-#TODO auth
+_ORGANIZATIONS = {}  # by slug
+def _get_org_groups(organization_slug):
+    """
+    memo-ize orgs--it'll be a long time before we're too big for memory
+    """
+    if organization_slug not in _ORGANIZATIONS:
+        _ORGANIZATIONS[organization_slug] = list(ReviewGroup.objects.filter(
+            organization__slug=organization_slug))
+    grps = _ORGANIZATIONS[organization_slug]
+    if not grps:
+        raise Http404("nope") #if the org doesn't have any review groups its dead
+    return grps
+
+def reviewgroup_auth(view_func):
+    """
+    Confirms that user is in a ReviewGroup of the appropriate organization.
+    Note: Assumes that `organization` is the first view parameter (after request)
+    """
+    def wrapped(request, organization, *args, **kw):
+        allowed = _get_org_groups(organization)
+        allowed_groups = set([x.group_id for x in allowed])
+        group_ids = set(request.user.groups.values_list('id', flat=True))
+        if not group_ids.intersection(allowed_groups) \
+           and not request.user.is_superuser:
+            return HttpResponseForbidden('nope')
+        return view_func(request, organization, *args, **kw)
+    return wrapped
+
+@reviewgroup_auth
 def save_review(request, organization, content_type):
     # save a review
     redis = get_redis_connection("default")
@@ -35,22 +64,26 @@ def save_review(request, organization, content_type):
     if request.method == 'POST':
         content_type = request.POST.get('content_type')
         pk = request.POST.get('pk')
-        decisions_str = request.POST.get('decisions','')
+        decisions_str = request.POST.get('decisions', '')
         log_message = request.POST.get('log')
-        if content_type and pk and len(decisions_str) >= 3 and ':' in decisions_str:
+        if content_type and pk and len(decisions_str) >= 3\
+           and ':' in decisions_str:
+            org = _get_org_groups(organization)
             ct = ContentType.objects.get_for_id(int(content_type))
             # make sure the object exists
             obj = ct.get_object_for_this_type(pk=pk)
             decisions = [d[:257].split(':') for d in decisions_str.split(';')]
             # 1. save to database
             reviews = [Review.objects.create(content_type=ct, object_id=obj.id,
+                                             organization_id=org[0].organization_id,
                                              reviewer=request.user,
                                              key=k, decision=decision)
-                       for k,decision in decisions]
+                       for k, decision in decisions]
 
             if log_message:
                 ReviewLog.objects.create(content_type=ct,
                                          object_id=obj.id,
+                                         organization_id=org[0].organization_id,
                                          reviewer=request.user,
                                          message=log_message)
             # 2. save to redis
@@ -65,7 +98,8 @@ def save_review(request, organization, content_type):
             return HttpResponse("ok")
     return HttpResponse("nope!")
 
-#TODO auth
+
+@reviewgroup_auth
 def get_review_history(request, organization):
     redis = get_redis_connection("default")
     reviewskey = '{}_reviews'.format(organization)
@@ -75,7 +109,7 @@ def get_review_history(request, organization):
     getlogs = request.GET.get('logs')
 
     pk_keys = ['{}_{}'.format(content_type_id, pk) for pk in pks]
-    reviews = redis.hmget(reviewskey, **pk_keys)
+    reviews = redis.hmget(reviewskey, *pk_keys)
     logs = []
     if getlogs:
         for pk in pks:
@@ -85,18 +119,19 @@ def get_review_history(request, organization):
                              'm': r['message'],
                              'ts': int(time.mktime(r['created_at'].timetuple()))
                          } for r in ReviewLog.objects.filter(
+                             organization__slug=organization,
                              content_type_id=content_type_id,
                              object_id=int(pk)
                          ).order_by('-id').values_list('reviewer__first_name',
                                                        'message',
-                                                       'created_at')
-                           ]})
-    return HttpResponse("""{"reviews":[{reviews}],"logs":[{logs}]}""".format(
-        reviews=','.join([o.encode('utf-8') for o in reviews]),
-        logs=json.dumps(logs)
+                                                       'created_at')]})
+    return HttpResponse("""{"reviews":[%s],"logs":[%s]}""" % (
+        ','.join([o.encode('utf-8') for o in reviews]),
+        json.dumps(logs)
     ), content_type='application/json')
 
-#TODO auth
+
+@reviewgroup_auth
 def mark_attention(request, organization, content_type, pk):
     # POST/DELETE mark whether someone is looking at this
     # takes the reviewer name from login username
@@ -118,25 +153,26 @@ def mark_attention(request, organization, content_type, pk):
 
 
 def _clear_old_marks(organization, max=50):
-    too_old = int(time.time()) - 60*60*2 #2 hours
+    too_old = int(time.time()) - 60*60*2  # 2 hours
     redis = get_redis_connection("default")
     rkey = '{}_marks'.format(organization)
-    marks = sorted([json.loads(v.decode('utf-8'))
-             for v in redis.hgetall(rkey).values()],
-                   key=lambda m:m[3], reverse=True)
-    to_delete = [m[2] for i,m in enumerate(marks)
+    marks = sorted(
+        [json.loads(v.decode('utf-8'))
+         for v in redis.hgetall(rkey).values()],
+        key=lambda m: m[3], reverse=True)
+    to_delete = [m[2] for i, m in enumerate(marks)
                  if i > max or m[3] < too_old]
     if to_delete:
         redis.hdel(rkey, *to_delete)
 
 
-#TODO auth
+@reviewgroup_auth
 def current_review_state(request, organization):
     """
     for polling fast updates
     {
       objects: [
-        {"type":"<event content type id>",
+        {"type":<event content type id>,
          "pk": 123456,
          "<review key>":"<decision>",...
         }, ...
@@ -154,15 +190,41 @@ def current_review_state(request, organization):
     if not items:
         # TODO: get them from db and save them to redis
         # if no items in db, then lpush empty string to items
-        pass
+        org = _get_org_groups(organization)
+        #maybe just get most recent review for each object
+        query = Review.objects.filter(organization_id=org[0].organization_id).order_by('-id')
+        try:
+            db_res = list(query.distinct('content_type', 'object_id', 'key')[:50])
+        except NotImplementedError:
+            #sqlite doesn't support 'DISTINCT ON' so we'll fake it
+            db_res = []
+            db_pre = list(query[:100])
+            already = set()
+            for r in db_pre:
+                key = (r.content_type_id, r.object_id, r.key)
+                if key not in already:
+                    db_res.append(r)
+                    already.add(key)
+        if not db_res:
+            # no keys, so to stop db hits every time, we push an empty
+            redis.lpush(itemskey, '')
+        else:
+            revs = OrderedDict()
+            for rev in db_res:
+                revs.setdefault(
+                    (rev.content_type_id, rev.object_id),
+                    {'type': rev.content_type_id,
+                     'pk': rev.object_id}).update({rev.key: rev.decision})
+            json_revs = [json.dumps(r) for r in revs.values()]
+            redis.lpush(itemskey, *json_revs)
     else:
         if items[-1] == b'':
-            #edge case of no items
-            # redis.rpop'ing here could race with other client hits
-            # so we will do that somewhere else
+            # edge case of no items (or had no items earlier)
+            # redis.rpop'ing here could race with other client hits so
+            # we will just ignore until the queue fills up and it gets bumped
             items.pop()
 
-    return HttpResponse("""{"objects":[{objects}],"marks":[{marks}]}""".format(
-        objects=','.join([o.decode('utf-8') for o in items]),
-        marks=','.join([m.decode('utf-8') for m in marks])
+    return HttpResponse("""{"objects":[%s],"marks":[%s]}""" % (
+        ','.join([o.decode('utf-8') for o in items]),
+        ','.join([m.decode('utf-8') for m in marks])
     ), content_type='application/json')
