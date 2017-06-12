3505912938e6f8a07b7dd224f830158933157f99
Schuyler Duveen <sky@skyb.us>
2017-06-12 17:49:21 -0400

    small tweaks for prod on connier

--- event_review/admin.py
+++ event_review/admin.py
@@ -56,7 +56,8 @@ class EventAdmin(admin.ModelAdmin):
 
     def get_actions(self, request):
         actions = super(EventAdmin, self).get_actions(request)
-        del actions['delete_selected']
+        if 'deleted_selected' in actions:
+            del actions['delete_selected']
         return actions
 
     def has_delete_permission(self, request, obj=None):
