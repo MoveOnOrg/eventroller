f08a664e9e00e44bd1223f7f9abcdfca36091f53
Sandra Chung <sandra@moveon.org>
2017-06-12 17:06:53 -0600

    now you can mark real and fake duplicates

--- event_exim/models.py
+++ event_exim/models.py
@@ -24,139 +24,163 @@ CRM_TYPES = {
 
 
 class EventSource(models.Model):
-   """
-   This represents a source of data to fill event_store.models.Event
+    """
+    This represents a source of data to fill event_store.models.Event
 
-   One question is how best to connect an eventsource to the Event
-   
-   """
-   name = models.CharField(max_length=128, help_text="e.g. campaign or just describe the system")
-   origin_organization = models.ForeignKey(Organization)
-   osdi_name = models.CharField(max_length=765)
+    One question is how best to connect an eventsource to the Event
 
-   crm_type = models.CharField(max_length=16, choices=[(k,k) for k in CRM_TYPES])
+    """
+    name = models.CharField(max_length=128, help_text="e.g. campaign or just describe the system")
+    origin_organization = models.ForeignKey(Organization)
+    osdi_name = models.CharField(max_length=765)
 
-   crm_data = JSONField(null=True, blank=True)
+    crm_type = models.CharField(max_length=16, choices=[(k,k) for k in CRM_TYPES])
 
-   #(provided: ping url for update (put this on your thanks page for event creation)
-   update_style = models.IntegerField(choices=(
-       (0, 'manual only'),
-       (1, 'ping'),
-       (2, 'ping with event reference'),
-       (3, 'daily pull'),
-       (4, 'hourly'),))
+    crm_data = JSONField(null=True, blank=True)
 
-   allows_updates = models.IntegerField(default=0, choices=((0,'no'), (1,'yes')), help_text='as sink')
+    #(provided: ping url for update (put this on your thanks page for event creation)
+    update_style = models.IntegerField(choices=(
+        (0, 'manual only'),
+        (1, 'ping'),
+        (2, 'ping with event reference'),
+        (3, 'daily pull'),
+        (4, 'hourly'),))
 
-   #(test connection button)
-   last_update = models.CharField(max_length=128, null=True, blank=True)
+    allows_updates = models.IntegerField(default=0, choices=((0,'no'), (1,'yes')), help_text='as sink')
 
-   def __str__(self):
-      return self.name
+    #(test connection button)
+    last_update = models.CharField(max_length=128, null=True, blank=True)
 
-   @property
-   def data(self):
-      """
-      Canonical data for this record -- sometimes this can be loaded from settings
-      so that the database doesn't need to store a password in plaintext
-      """
-      settings_data = getattr(settings, 'EVENT_SOURCES', {})
-      return settings_data.get(self.name, self.crm_data)
+    def __str__(self):
+        return self.name
 
-   @cached_property
-   def api(self):
-      if self.crm_type:
-         connector_module = importlib.import_module('event_exim.connectors.%s' % self.crm_type)
-         return connector_module.Connector(self)
+    @property
+    def data(self):
+        """
+            Canonical data for this record -- sometimes this can be loaded from settings
+            so that the database doesn't need to store a password in plaintext
+        """
+        settings_data = getattr(settings, 'EVENT_SOURCES', {})
+        return settings_data.get(self.name, self.crm_data)
 
-   def update_events(self, update_since=None):
-      """
-      Sync events from source to local database
-      """
-      # 1. load events from our connector
-      if update_since is None:
-         update_since = self.last_update
-      event_data = self.api.load_events(last_updated=update_since)
+    @cached_property
+    def api(self):
+        if self.crm_type:
+            connector_module = importlib.import_module('event_exim.connectors.%s' % self.crm_type)
+            return connector_module.Connector(self)
 
-      all_events = {str(e['organization_source_pk']):e for e in event_data['events']}
-      new_host_ids = set([e['organization_host'].member_system_pk for e in all_events.values()])
-      existing = list(Event.objects.filter(organization_source_pk__in=all_events.keys(),
-                                           organization_source=self))
-      # 2. save hosts, new and existing Activist records
-      host_update_fields = ('hashed_email', 'email', 'name', 'phone')
-      existing_hosts = {a.member_system_pk:a
-                        for a in Activist.objects.filter(member_system=self,
-                                                         member_system_pk__in=new_host_ids)}
-      for e in all_events.values():
-         ehost = e.get('organization_host')
-         if ehost:
-            host_by_pk = existing_hosts.get(ehost.member_system_pk)
-            if host_by_pk:
-               ehost.id = host_by_pk.id
-               for hf in host_update_fields:
-                  if getattr(host_by_pk, hf) != getattr(ehost, hf):
-                     ehost.save()
-                     break #inner loop
-            else:
-               ehost.save()
+    def update_events(self, update_since=None):
+        """
+        Sync events from source to local database
+        """
+        # 1. load events from our connector
+        if update_since is None:
+            update_since = self.last_update
+        event_data = self.api.load_events(last_updated=update_since)
 
-      # 3. bulk-create all new events not in the system yet
-      existing_ids = set([e.organization_source_pk for e in existing])
-      new_events = [Event(**e) for e in all_events.values()
-                    if e['organization_source_pk'] not in existing_ids]
-      Event.objects.bulk_create(new_events)
+        all_events = {str(e['organization_source_pk']):e for e in event_data['events']}
+        new_host_ids = set([e['organization_host'].member_system_pk for e in all_events.values()])
+        existing = list(Event.objects.filter(organization_source_pk__in=all_events.keys(),
+                                             organization_source=self))
+        # 2. save hosts, new and existing Activist records
+        host_update_fields = ('hashed_email', 'email', 'name', 'phone')
+        existing_hosts = {a.member_system_pk:a
+                          for a in Activist.objects.filter(member_system=self,
+                                                           member_system_pk__in=new_host_ids)}
+        for e in all_events.values():
+            ehost = e.get('organization_host')
+            if ehost:
+                host_by_pk = existing_hosts.get(ehost.member_system_pk)
+                if host_by_pk:
+                    ehost.id = host_by_pk.id
+                    for hf in host_update_fields:
+                        if getattr(host_by_pk, hf) != getattr(ehost, hf):
+                            ehost.save()
+                            break #inner loop
+                else:
+                    ehost.save()
 
-      # 4. save any changes to existing events
-      for e in existing:
-         self.update_event(e, all_events[e.organization_source_pk])
-      # 5. now that we've updated things, save this EventSource record with last_updated
-      self.last_update = event_data['last_updated']
-      self.save()
+        # 3. bulk-create all new events not in the system yet
+        existing_ids = set([e.organization_source_pk for e in existing])
+        new_events = [Event(**e) for e in all_events.values()
+                      if e['organization_source_pk'] not in existing_ids]
+        Event.objects.bulk_create(new_events)
 
-   def update_event(self, event, new_event_dict):
-      changed = False
-      for k,v in new_event_dict.items():
-         if k == 'organization_host' and event.organization_host:
-            if not event.organization_host.likely_same(v):
-               event.organization_host = v
-               changed = True
-         else:
-            if getattr(event,k) != v:
-               setattr(event,k,v)
-               changed = True
-      if changed:
-         event.save()
-      return changed
+        # 4. save any changes to existing events
+        for e in existing:
+            self.update_event(e, all_events[e.organization_source_pk])
+        # 5. now that we've updated things, save this EventSource record with last_updated
+        self.last_update = event_data['last_updated']
+        self.save()
+
+        def update_event(self, event, new_event_dict):
+            changed = False
+            for k,v in new_event_dict.items():
+                if k == 'organization_host' and event.organization_host:
+                    if not event.organization_host.likely_same(v):
+                        event.organization_host = v
+                        changed = True
+                else:
+                    if getattr(event,k) != v:
+                        setattr(event,k,v)
+                        changed = True
+            if changed:
+                event.save()
+            return changed
 
 class EventDupeManager(models.Manager):
-  def create_event_dupe(self, source_event, dupe_event, decision = 0):
-    event_dupe = self.create(source_event = source_event, dupe_event = dupe_event, decision = 0)
-    event_dupe.save()
-    return event_dupe.id
+    def create_event_dupe(self, source_event, dupe_event, decision = 0):
+        event_dupe = self.create(source_event = source_event, dupe_event = dupe_event, decision = 0)
+        event_dupe.save()
+        return event_dupe.id
 
 class EventDupeGuesses(models.Model):
-  source_event = models.ForeignKey(Event, related_name='dupe_guesses')
-  dupe_event = models.ForeignKey(Event, related_name='dupe_guess_sources')
+    source_event = models.ForeignKey(Event, related_name='dupe_guesses')
+    dupe_event = models.ForeignKey(Event, related_name='dupe_guess_sources')
 
-  decision = models.IntegerField(choices=( (0, 'undecided'),
+    decision = models.IntegerField(choices=( (0, 'undecided'),
                                            (1, 'not a duplicate'),
-                                           (2, 'yes, duplicates')))
-  objects = EventDupeManager()
+                                           (2, 'yes, duplicates')), verbose_name = 'Status')
+
+    objects = EventDupeManager()
+    class Meta:
+        unique_together = (('source_event','dupe_event'),)
   
-  class Meta:
-    unique_together = (('source_event','dupe_event'),)
+    def source_event_summary(self):
+        source_event = '{}: {}. {}, {}, {}. "{}"'.format(
+            self.source_event.id,
+            self.source_event.title,
+            self.source_event.venue,
+            self.source_event.city,
+            self.source_event.state,
+            self.source_event.public_description
+        )
+        return source_event
+    source_event_summary.short_description = 'Original Event'
+
+    def dupe_event_summary(self):
+        dupe_event = '{}: {}. {}, {}, {}. "{}"'.format(
+            self.dupe_event.id,
+            self.dupe_event.title,
+            self.dupe_event.venue,
+            self.dupe_event.city,
+            self.dupe_event.state,
+            self.dupe_event.public_description
+        )
+        return dupe_event
+    dupe_event_summary.short_description = 'Duplicate Event'
 
 class Org2OrgShare(models.Model):
-  event_source = models.ForeignKey(EventSource, related_name='share_sources')
-  event_sink = models.ForeignKey(EventSource, related_name='share_sinks')
+    event_source = models.ForeignKey(EventSource, related_name='share_sources')
+    event_sink = models.ForeignKey(EventSource, related_name='share_sinks')
 
-  status = models.IntegerField(choices=( (-1, 'disabled'),
-                                         (0, 'offered'),
-                                         (1, 'enabled')))
+    status = models.IntegerField(choices=( (-1, 'disabled'),
+                                           (0, 'offered'),
+                                           (1, 'enabled')))
 
-  filters = JSONField(null=True, blank=True)
+    filters = JSONField(null=True, blank=True)
 
-  created_at = models.DateTimeField(auto_now_add=True)
-  modified_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
-  creator = models.ForeignKey(User)
+    creator = models.ForeignKey(User)
