d0f13a7e90331deb5f75e717e7a1f9c5d8ccddb7
Americo <ame.zuz@gmail.com>
2018-10-09 16:35:14 -0700

    First pass at collapsible messages

--- reviewer/static/js/reviewer.js
+++ reviewer/static/js/reviewer.js
@@ -156,7 +156,7 @@ Reviewer.prototype = {
       if (selectMode === 'multiselect' && Object.keys(reviewSubject.data).length === 0) {
         // handle case in tag multiselect where all tags are removed
         decisions.push(name + ':' + '');
-      } else if (name in reviewSubject.data) { 
+      } else if (name in reviewSubject.data) {
         // reviewSubject.data gets updated in postRender save button listener
         decisions.push(name + ':' + reviewSubject.data[name]);
       }
@@ -309,7 +309,7 @@ Reviewer.prototype = {
     for (var i=0,l=pks.length; i<l; i++) {
       var reviewSubject = this.state[pks[i]];
       if (reviewSubject.o && reviewSubject.data) {
-        reviewSubject.o.innerHTML = this.render(reviewSubject, this.opt.selectMode); 
+        reviewSubject.o.innerHTML = this.render(reviewSubject, this.opt.selectMode);
         this.postRender(reviewSubject, this.opt.selectMode);
       }
     }
@@ -322,7 +322,7 @@ Reviewer.prototype = {
             + '<div class="review-widget">'
             + ' <div class="row">'
             + '  <div class="col-md-10">'
-            + (selectMode === 'multiselect' 
+            + (selectMode === 'multiselect'
                 ? self.renderDecisionsMulti(this.opt.schema, reviewSubject)
                 : this.opt.schema.map(
                   (schema) => self.renderDecisions(schema, reviewSubject)
@@ -426,8 +426,9 @@ Reviewer.prototype = {
                               ? ('<span class="glyphicon glyphicon-list-alt" style="padding:0 5px 0 5px" '
                                  + ' title="bulk note"></span>')
                               : '')))
+      const collapseClass = log.t === 'message' ? 'collapsed' : ''
       return (''
-              + '<div class="logitem"'
+              + `<div class="logitem ${collapseClass}"`
               + ((other && log.pk) ? ' data-pk="'+log.pk+'" style="background-color: hsl('+hue+',17%,80%)"' : '')
               + '>'
               + ((other && log.pk) ? '-- ' : '')
@@ -497,9 +498,9 @@ Reviewer.prototype = {
           + tag_data[key].map(function(o){
             return (''
               + '          <option ' + ((reviewSubject.data[o.id])?'selected="selected"':'')
-              + ' value="' + o['record'] + '" data-name="' + o['id'] + '">' 
+              + ' value="' + o['record'] + '" data-name="' + o['id'] + '">'
               + o['display'] + '</option>'
-            )        
+            )
           }).join('')
           + '        </optgroup>'
         )
@@ -570,7 +571,7 @@ Reviewer.prototype = {
       // 3. saveReview()
       if (changed) {
         self.saveReview(
-          reviewSubject, selectMode, log || undefined, visibility || undefined, 
+          reviewSubject, selectMode, log || undefined, visibility || undefined,
           function() {
             // 4. on callback: add status (and clear log message)
             self.renderSaveUpdate(reviewSubject);
@@ -590,6 +591,9 @@ Reviewer.prototype = {
       this.loadChosen();
     }
 
+    // 5 Add click listener to messages so that they can be collapsed/expanded
+    self.addCollapseListeners()
+
   },
   addDeleteListeners: function(reviewSubject) {
     // Delete reviews (notes)
@@ -608,5 +612,14 @@ Reviewer.prototype = {
   },
   loadChosen: function() {
     $('.chosen-select').chosen({width: "100%", display_selected_options: false});
+  },
+  addCollapseListeners: function() {
+    const collapsibleItems = document.querySelectorAll('div.collapsed')
+
+    collapsibleItems.forEach(log => {
+      log.addEventListener('click', e => {
+        log.classList.toggle('collapsed')
+      })
+    })
   }
 };
